4bad5fc2da73e429bde26e8f905a9ba153b2913e
Max E <maxeisen98@web.de>
2020-05-17 23:47:26 +0200

    fixed bug of clearing the board

--- src/app/grid/grid.component.ts
+++ src/app/grid/grid.component.ts
@@ -75,9 +75,7 @@ export class GridComponent implements OnInit {
     else{
       document.getElementById('btn-visualize').textContent = 'Visualizing...';
       document.getElementById('btn-visualize').style.backgroundColor = '#ff0000';
-      if(this.checkVisited() || this.checkClosed()){
-        this.clearVisitedNodes();
-      }
+      this.clearVisitedNodes();
       isRunning = true;
       this.visualizeAlgorithm();
     }
